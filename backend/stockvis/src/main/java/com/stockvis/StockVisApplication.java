7d146afe6cb76bce2950e8ebccf47428d1d6864c
David Weaver <davidweav322@gmail.com>
2024-11-12 14:34:31 -0500

    Created Spring Boot Project

---
+++ backend/stockvis/src/main/java/com/stockvis/StockVisApplication.java
@@ -0,0 +1,13 @@
+package com.stockvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockVisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockVisApplication.class, args);
+	}
+
+}
