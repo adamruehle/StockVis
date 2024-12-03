f83dd3d42eef4c4071d4c22b7960825f5508d2d0
David Weaver <davidweav322@gmail.com>
2024-12-03 18:37:46 -0500

    adds dividends

--- backend/stockvis/src/main/java/com/stockvis/repository/DividendRepository.java
+++ backend/stockvis/src/main/java/com/stockvis/repository/DividendRepository.java
@@ -1,5 +1,7 @@
 package com.stockvis.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.stockvis.entity.DividendId;
 
 @Repository
 public interface DividendRepository extends JpaRepository<Dividend, DividendId> {
+
+    List<Dividend> findByTicker(String ticker);
 
 }
